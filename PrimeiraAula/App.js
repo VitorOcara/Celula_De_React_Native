ce475be91ea91ae54528cbc33800302eb6295eab
VitorOcara <vitorocara2001@gmail.com>
2023-09-20 13:35:10 -0300

    Primeira Aula

---
+++ PrimeiraAula/App.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+      <TextInput style={styles.imput} />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imput: {
+    backgroundColor: "red",
+  }
+});
